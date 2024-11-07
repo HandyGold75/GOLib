4a9d202a1f64530dc960a40d0156ffc8fd1f5b1e
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-11-07 16:39:19 +0100

    07-Nov-24

--- args/args.go
+++ args/args.go
@@ -10,38 +10,24 @@ import (
 )
 
 type (
-	// Field:  `switch:"[<switch>,...]" prefix:"-" opts:"[posistional,required]" default:"<value>" help:"<message>"`
-	// Example: `switch:"s,-switch,-switch-me" opts:"required" default:"hello" help:"hello world"`
-	// Usage:   -<switch> <value>
-	//          --<switch>=<value>
 	arg struct {
-		FieldA string  `switch:"a"         help:"Some help for a."`
+		Help   bool    `switch:"h,-help"   opts:"help"`
+		FieldA float64 `switch:"a"         opts:"posistional"`
 		FieldB bool    `switch:"b"         prefix:"/" opts:"required"`
 		FieldC int     `switch:"c,-cc"     default:"5"`
 		FieldD uint    `switch:"d,-dd-ddd" default:"7" opts:"required"`
-		FieldE float64 `switch:"e,-ee"     opts:"posistional"`
-		Help   bool    `switch:"h,-help"   opts:"help"`
+		FieldE string  `switch:"e,-ee"     help:"Some help for e."`
 	}
 )
 
-// Shows help menu related to s. Panics if s is not of type struct. Private struct fields are ignored.
-func HelpMenu[T any](s T) {
-	tOf := reflect.TypeOf(s)
+func forEachStructField[T any](s *T, handler func(reflect.StructField, reflect.Value)) {
+	tOf := reflect.TypeOf(*s)
 	if tOf.Kind() != reflect.Struct {
 		panic("s is not a stuct")
 	}
-	vOf := reflect.ValueOf(&s).Elem()
-	if tOf.Kind() != reflect.Struct {
+	vOf := reflect.ValueOf(s).Elem()
+	if vOf.Kind() != reflect.Struct {
 		panic("s is not a stuct")
-	}
-
-	maxLenSwts := 0
-	for i := 0; i < tOf.NumField(); i++ {
-		tOfField := tOf.Field(i)
-		lenSwt := len(tOfField.Tag.Get("switch")) + max(1, len(tOfField.Tag.Get("prefix"))) + (len(strings.Split(tOfField.Tag.Get("switch"), ",")))
-		if lenSwt > maxLenSwts {
-			maxLenSwts = lenSwt
-		}
 	}
 
 	for i := 0; i < tOf.NumField(); i++ {
@@ -50,30 +36,79 @@ func HelpMenu[T any](s T) {
 		if !vOfField.CanSet() {
 			continue
 		}
+		if tOfField.Tag.Get("switch") == "" {
+			panic("no switch specified for field " + tOfField.Name)
+		}
+		handler(tOfField, vOfField)
+	}
+}
 
-		prefix := tOfField.Tag.Get("prefix")
+// Shows help menu related to s. Panics if s is not of type struct. Private struct fields are ignored.
+func HelpMenu[T any](s T, details bool) {
+	maxLenSwts := 0
+	forEachStructField(&s, func(field reflect.StructField, _ reflect.Value) {
+		lenSwt := len(field.Tag.Get("switch")) + max(1, len(field.Tag.Get("prefix"))) + (len(strings.Split(field.Tag.Get("switch"), ",")))
+		if lenSwt > maxLenSwts {
+			maxLenSwts = lenSwt
+		}
+	})
+
+	helpMenuStr := ""
+	argsStr := ""
+
+	forEachStructField(&s, func(field reflect.StructField, _ reflect.Value) {
+		prefix := field.Tag.Get("prefix")
 		if prefix == "" {
 			prefix = "-"
 		}
 
 		swts := ""
-		for _, swt := range strings.Split(tOfField.Tag.Get("switch"), ",") {
+		for _, swt := range strings.Split(field.Tag.Get("switch"), ",") {
 			swts += " " + prefix + swt
 		}
 
-		fmt.Println(tOfField.Name)
-		fmt.Printf(" %-"+strconv.Itoa(maxLenSwts)+"v  %-9v", swts, "<"+tOfField.Type.Name()+">")
-		if tOfField.Tag.Get("opts") != "" {
-			fmt.Printf(" (%v)", tOfField.Tag.Get("opts"))
+		switch field.Type.Name() {
+		case "string":
+			argsStr += " [" + prefix + strings.Split(field.Tag.Get("switch"), ",")[0] + " <" + field.Type.Name() + ">]"
+
+		case "bool":
+			argsStr += " [" + prefix + strings.Split(field.Tag.Get("switch"), ",")[0] + "]"
+
+		case "int", "int8", "int16", "int32", "int64":
+			argsStr += " [" + prefix + strings.Split(field.Tag.Get("switch"), ",")[0] + " <" + field.Type.Name() + ">]"
+
+		case "uint", "uint8", "uint16", "uint32", "uint64", "uintptr":
+			argsStr += " [" + prefix + strings.Split(field.Tag.Get("switch"), ",")[0] + " <" + field.Type.Name() + ">]"
+
+		case "float32", "float64":
+			argsStr += " [" + prefix + strings.Split(field.Tag.Get("switch"), ",")[0] + " <" + field.Type.Name() + ">]"
+
+		default:
+			argsStr += " [" + prefix + strings.Split(field.Tag.Get("switch"), ",")[0] + "]"
 		}
-		fmt.Println()
-		if tOfField.Tag.Get("help") != "" {
-			fmt.Printf("	%v\r\n", tOfField.Tag.Get("help"))
+
+		helpMenuStr += fmt.Sprintf("%v\r\n %-"+strconv.Itoa(maxLenSwts)+"v  %-9v", field.Name, swts, "<"+field.Type.Name()+">")
+		if field.Tag.Get("opts") != "" {
+			helpMenuStr += fmt.Sprintf(" (%v)", field.Tag.Get("opts"))
 		}
+		helpMenuStr += "\r\n"
+		if field.Tag.Get("help") != "" {
+			helpMenuStr += fmt.Sprintf("	%v\r\n", field.Tag.Get("help"))
+		}
+	})
+
+	execPath, err := os.Executable()
+	if err != nil {
+		execPath = "exec"
+	}
+	execPathSplit := strings.Split(strings.ReplaceAll(execPath, "\\", "/"), "/")
+	fmt.Print("Usage: " + execPathSplit[len(execPathSplit)-1] + argsStr + "\r\n")
+	if details {
+		fmt.Print("\r\n" + helpMenuStr)
 	}
 }
 
-// Parses os.Args into s. Panics if s is not of type struct. Private struct fields are ignored.
+// Parses os.Args into s. Panics if s is not of type struct or a public field doesn't contain a switch tag. Private struct fields are ignored.
 //
 // Struct format:
 //
@@ -100,119 +135,160 @@ func HelpMenu[T any](s T) {
 // Example:
 //
 //	arg struct {
-//		FieldA string  `switch:"a"         help:"Some help for a."`
+//		Help   bool    `switch:"h,-help"   opts:"help"`
+//		FieldA float64 `switch:"a"         opts:"posistional"`
 //		FieldB bool    `switch:"b"         prefix:"/" opts:"required"`
 //		FieldC int     `switch:"c,-cc"     default:"5"`
 //		FieldD uint    `switch:"d,-dd-ddd" default:"7" opts:"required"`
-//		FieldE float64 `switch:"e,-ee"     opts:"posistional"`
-//		Help   bool    `switch:"h,-help"   opts:"help"`
+//		FieldE string  `switch:"e,-ee"     help:"Some help for e."`
 //	}
 //
 // Usage: exec -a=somemessage /b --cc 10 --dd-ddd=2
 func ParseArgs[T any](s T) T {
 	args := os.Args[1:]
-	tOf := reflect.TypeOf(s)
-	if tOf.Kind() != reflect.Struct {
-		panic("s is not a stuct")
-	}
-	vOf := reflect.ValueOf(&s).Elem()
-	if tOf.Kind() != reflect.Struct {
-		panic("s is not a stuct")
-	}
 
-	for i := 0; i < tOf.NumField(); i++ {
-		tOfField := tOf.Field(i)
-		vOfField := vOf.Field(i)
-		if !vOfField.CanSet() {
+	allSwitches := [][2]string{}
+	allPrefixes := []string{}
+	forEachStructField(&s, func(field reflect.StructField, value reflect.Value) {
+		prefix := field.Tag.Get("prefix")
+		if prefix == "" {
+			prefix = "-"
+		}
+
+		for _, swt := range strings.Split(field.Tag.Get("switch"), ",") {
+			allSwitches = append(allSwitches, [2]string{prefix, swt})
+			if !slices.Contains(allPrefixes, prefix) {
+				allPrefixes = append(allPrefixes, prefix)
+			}
+		}
+	})
+
+	fmt.Println(args)
+
+	for i := 0; i < len(args); i++ {
+		if strings.Contains(args[i], "=") {
+			continue
+		}
+		if slices.ContainsFunc(allSwitches, func(item [2]string) bool { return item[0]+item[1] == args[i] }) {
 			continue
 		}
 
-		if tOfField.Tag.Get("switch") == "" {
-			panic("no switch specified for field " + tOfField.Name)
+		prefix := ""
+		for _, item := range allSwitches {
+			if strings.HasPrefix(args[i], item[0]) {
+				prefix = item[0]
+				break
+			}
+		}
+		if prefix == "" {
+			continue
 		}
 
-		prefix := tOfField.Tag.Get("prefix")
+		newArgs := append([]string{}, args[:i]...) // Deepcopy
+		for _, a := range strings.Split(strings.Replace(args[i], prefix, "", 1), "") {
+			newArgs = append(newArgs, prefix+a)
+		}
+		args = append(newArgs, args[i+1:]...)
+		i = i + (len(newArgs) - 1)
+	}
+
+	fmt.Println(args)
+
+	forEachStructField(&s, func(field reflect.StructField, value reflect.Value) {
+		prefix := field.Tag.Get("prefix")
 		if prefix == "" {
 			prefix = "-"
 		}
 
 		index := -1
-		for _, swt := range strings.Split(tOfField.Tag.Get("switch"), ",") {
+		for _, swt := range strings.Split(field.Tag.Get("switch"), ",") {
 			index = slices.IndexFunc(args, func(a string) bool { return a == prefix+swt || strings.HasPrefix(a, prefix+swt+"=") })
 			if index > -1 {
-				if slices.Contains(strings.Split(tOfField.Tag.Get("opts"), ","), "help") {
-					HelpMenu(s)
+				if slices.Contains(strings.Split(field.Tag.Get("opts"), ","), "help") {
+					HelpMenu(s, true)
 					os.Exit(0)
 				}
 				break
 			}
 		}
 
-		value := tOfField.Tag.Get("default")
+		val := field.Tag.Get("default")
 		if index > -1 {
 			if strings.Contains(args[index], "=") {
-				value = strings.Split(args[index], "=")[1]
-			} else if index+1 < len(args) && !strings.HasPrefix(args[index+1], prefix) {
-				value = args[index+1]
+				val = strings.Split(args[index], "=")[1]
+			} else if index+1 < len(args) && !slices.ContainsFunc(allPrefixes, func(item string) bool { return strings.HasPrefix(args[index+1], item) }) {
+				val = args[index+1]
 				args = slices.Delete(args, index, index+1)
-			} else if tOfField.Type.Name() == "bool" {
-				value = strconv.FormatBool(index != -1)
+			} else if field.Type.Name() == "bool" {
+				val = strconv.FormatBool(index != -1)
 			}
 			args = slices.Delete(args, index, index+1)
 		}
 
-		if value == "" && tOfField.Type.Name() != "bool" {
-			if slices.Contains(strings.Split(tOfField.Tag.Get("opts"), ","), "required") {
-				fmt.Println("Missing required argument " + tOfField.Name + "\r\n")
-				HelpMenu(s)
+		if val == "" && field.Type.Name() != "bool" {
+			if slices.Contains(strings.Split(field.Tag.Get("opts"), ","), "required") {
+				fmt.Println("Missing required argument " + field.Name + "\r\n")
+				HelpMenu(s, false)
+				os.Exit(0)
+			} else if index > -1 {
+				fmt.Println("Missing argument value for " + field.Name + "\r\n")
+				HelpMenu(s, false)
 				os.Exit(0)
 			}
-			continue
+			return
 		}
 
-		switch tOfField.Type.Name() {
+		switch field.Type.Name() {
 		case "string":
-			vOfField.SetString(value)
+			value.SetString(val)
 
 		case "bool":
-			vOfField.SetBool(value == "true")
+			value.SetBool(val == "true")
 
 		case "int", "int8", "int16", "int32", "int64":
-			val, err := strconv.ParseInt(value, 10, 64)
+			v, err := strconv.ParseInt(val, 10, 64)
 			if err != nil {
-				fmt.Println("Invalid int format " + value + "\r\n")
-				HelpMenu(s)
+				fmt.Println("Invalid int format '" + val + "'\r\n")
+				HelpMenu(s, false)
 				os.Exit(0)
 			}
-			vOfField.SetInt(val)
+			value.SetInt(v)
 
 		case "uint", "uint8", "uint16", "uint32", "uint64", "uintptr":
-			val, err := strconv.ParseUint(value, 10, 64)
+			v, err := strconv.ParseUint(val, 10, 64)
 			if err != nil {
-				fmt.Println("Invalid uint format " + value + "\r\n")
-				HelpMenu(s)
+				fmt.Println("Invalid uint format '" + val + "'\r\n")
+				HelpMenu(s, false)
 				os.Exit(0)
 			}
-			vOfField.SetUint(val)
+			value.SetUint(v)
 
 		case "float32", "float64":
-			val, err := strconv.ParseFloat(value, 64)
+			v, err := strconv.ParseFloat(val, 64)
 			if err != nil {
-				fmt.Println("Invalid float format " + value + "\r\n")
-				HelpMenu(s)
+				fmt.Println("Invalid float format '" + val + "'\r\n")
+				HelpMenu(s, false)
 				os.Exit(0)
 			}
-			vOfField.SetFloat(val)
+			value.SetFloat(v)
 
 		default:
-			panic("unsuported type " + tOfField.Type.Name())
+			panic("unsuported type " + field.Type.Name())
 		}
+
+	})
+
+	fmt.Println(args)
+	if len(args) > 0 {
+		fmt.Println("Unknown arguments '" + strings.Join(args, "', '") + "'\r\n")
+		HelpMenu(s, false)
+		os.Exit(0)
 	}
 
 	return s
 }
 
 func main() {
-	arg := ParseArgs(arg{})
-	fmt.Println(arg)
+	args := ParseArgs(arg{})
+	fmt.Println(args)
 }
