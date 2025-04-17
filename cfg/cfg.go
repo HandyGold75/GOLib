900b1855081498357e7f131f906833428886990d
HandyGold75 <IZO@HandyGold75.com>
2025-04-17 11:06:00 +0200

    17-Apr-25

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -11,13 +11,32 @@ import (
 // If config file is not present then it tries creating it with content of data.
 //
 // Config file is stored in `./golib/<name>.json` relative to `os.UserConfigDir`.
-func Load(name string, data any) error {
+func Load(name, data string) error {
 	file, err := os.UserConfigDir()
 	if err != nil {
 		return err
 	}
-	file += "/golib/" + name + ".json"
+	return LoadAbs(file+"/golib/"+name+".json", data)
+}
 
+// Loads config file into data.
+//
+// If config file is not present then it tries creating it with content of data.
+//
+// Config file is stored in `./<name>.json` relative to `os.Executable`.
+func LoadRel(name, data string) error {
+	file, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	fileSplit := strings.Split(strings.ReplaceAll(file, "\\", "/"), "/")
+	return LoadAbs(strings.Join(fileSplit[:len(fileSplit)-1], "/")+"/"+name+".json", data)
+}
+
+// Loads config file into data.
+//
+// If config file is not present then it tries creating it with content of data.
+func LoadAbs(file string, data any) error {
 	bytes, err := os.ReadFile(file)
 	if os.IsNotExist(err) || len(bytes) == 0 {
 		fileSplit := strings.Split(strings.ReplaceAll(file, "\\", "/"), "/")
@@ -49,13 +68,32 @@ func Load(name string, data any) error {
 // If config file is not present then it tries creating it with content of data.
 //
 // Config file is stored in `./golib/<name>.json` relative to `os.UserConfigDir`.
-func Dump(name string, data any) error {
+func Dump(name, data string) error {
 	file, err := os.UserConfigDir()
 	if err != nil {
 		return err
 	}
-	file += "/golib/" + name + ".json"
+	return DumpAbs(file+"/golib/"+name+".json", data)
+}
 
+// Dumps data to config file.
+//
+// If config file is not present then it tries creating it with content of data.
+//
+// Config file is stored in `./<name>.json` relative to `os.Executable`.
+func DumpRel(name, data string) error {
+	file, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	fileSplit := strings.Split(strings.ReplaceAll(file, "\\", "/"), "/")
+	return DumpAbs(strings.Join(fileSplit[:len(fileSplit)-1], "/")+"/"+name+".json", data)
+}
+
+// Dumps data to config file.
+//
+// If config file is not present then it tries creating it with content of data.
+func DumpAbs(file string, data any) error {
 	bytes, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
